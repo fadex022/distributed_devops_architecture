554ff6c477fbae9403f285a0d97ab16eac001d19
Fadel Bidiga <bidigafadel@gmail.com>
2023-12-18 23:12:31 +0000

    first commit

--- windows/bootstrap.ps1
+++ windows/bootstrap.ps1
@@ -4,5 +4,11 @@ if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+# Check if Docker is installed
+if (!(Get-Command docker -ErrorAction SilentlyContinue)) {
+    # Install Docker using Chocolatey
+    choco install docker-desktop -y
+}
+
 # Install Minikube using Chocolatey
 choco install minikube -y
